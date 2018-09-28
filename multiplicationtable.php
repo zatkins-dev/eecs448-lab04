8862e9cff736d0427bfd7a7811688bb9a315c747
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-28 13:40:23 -0500

    Added style

--- multiplicationtable.php
+++ multiplicationtable.php
@@ -1,4 +1,18 @@
 <?php
+    echo "<head>
+    <title>
+        Lab 04
+    </title>
+    <link rel=\"stylesheet\" href=\"../../style.css\"/>
+</head>
+<header>
+    <nav>
+        <a href=\"../../index.html\">Home</a>
+        &#x2712;
+        <a href=\"index.html\">Lab 04</a>
+    </nav>
+</header>";
+
     function getEntry($a, $b) {
         $prod = $a * $b;
         return ("<td align=\"center\">{$prod}</td>");
