f11fea7f3671c4175f4afdcbc37ef2d41a094d9e
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-10-04 14:32:33 -0500

    Fixed php so that text is displayed

--- quiz.php
+++ quiz.php
@@ -1,14 +1,29 @@
 <?php
     $q = array($_POST["q1"], $_POST["q2"], $_POST["q3"],$_POST["q4"],$_POST["q5"]);
-    $q_k = array("q1_c", "q2_c", "q3_a", "q4_d", "q5_a");
-
-    $correct = 0;
-
+    $q_k = array("C", "C", "A", "D", "A");
+    var_dump($_POST);
+    $correct = array(FALSE,FALSE,FALSE,FALSE,FALSE);
+    $ncorrect = 0;
     for ($i = 0; $i<5; $i++) {
-        if ($q1 == $q1_k) {
-            $correct++;
+        $qNum = $i+1;
+        echo "<div>";
+        echo "<p>";
+        echo "Number $qNum:";
+        echo "<br>";
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;Your answer: {$q[$i]}";
+        echo "<br>";
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;Correct Answer: {$q_k[$i]}";
+        echo "<br>";
+        if ($q[$i] == $q_k[$i]) {
+            $correct[$i] = TRUE;
+            $ncorrect++;
         }
+        $text = $correct[$i] ? 'correct' : 'incorrect';
+        echo "Number $qNum is $text";
+        echo "</p>";
+        echo "</div>";
     }
-    
+    $score = (100*$ncorrect)/5;
 
+    echo "\n$score";
 ?>
