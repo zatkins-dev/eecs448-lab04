64eb2ba51bcfd2900ba4f7933ed1996fbfb5d4a8
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-10-05 00:21:48 -0500

    added validation

---
+++ formChecker.js
@@ -0,0 +1,49 @@
+function checkForm() {
+    let username = document.getElementById("user");
+    let password = document.getElementById("pass");
+
+    let items = [document.getElementById("shoeCheck"), document.getElementById("penCheck"), document.getElementById("rockCheck")];
+    let itemCounts = [document.getElementById("numShoes"), document.getElementById("numPens"), document.getElementById("numRocks")];
+
+    let shipping = [document.getElementById("sevenday"), document.getElementById("threeday"), document.getElementById("overnight")];
+
+    let loginBlank = true;
+    if (username.value && password.value) {
+        loginBlank = false;
+        alert("Login info blank");
+    }
+    let properEmail = true;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(username.value)) {
+        properEmail = false;
+        alert("Username not in the form of an email address");
+    }
+    let shippingSelection = false;
+    for (let r of shipping) {
+        if (r.checked) {
+            shippingSelection = true;
+        }
+    }
+    if (!shippingSelection) alert("Select a shipping option");
+    let anItemSelected = false;
+    for (let item of items) {
+        if (r.checked) {
+            anItemSelected = true;
+        }
+    }
+    let countsCorrect = true;
+    for (let count of itemCounts) {
+        if (count.value !== math.floor(count.value)) {
+            countsCorrect = false;
+            alert("Must have an integer count");
+        }
+        if (count.value < 0) {
+            countsCorrect = false;
+            alert("Must have positive count");
+        }
+    }
+    if (loginBlank && properEmail && shippingSelection && anItemSelected && countsCorrect) {
+        return true;
+    }
+    event.preventDefault();
+    return false;
+}
